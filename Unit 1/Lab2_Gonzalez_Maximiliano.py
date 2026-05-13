f8199aef826b87ec47d9669d3229537c5d2f59fb
Bubbythy <tejapylo@gmail.com>
2026-05-13 09:19:07 -0600

    Add files via upload

---
+++ Unit 1/Lab2_Gonzalez_Maximiliano.py
@@ -0,0 +1,26 @@
+def multiply(a, b):
+   return a * b
+print(multiply(3,4))
+
+
+counter = 0
+for i in range(4):
+    counter = counter + 1
+    print(counter)
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Adult")
+"""""
+in here i put the : after the if age >= 18
+"""""
+
+a = 3
+b = 4
+
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
+
